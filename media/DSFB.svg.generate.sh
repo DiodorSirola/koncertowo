adee31b6b506354f67b12c3ebc35817b6bcf926b
Diodor Sirola <diodor.sirola@gmail.com>
2019-05-16 19:01:03 +0200

    Add missing media files

---
+++ media/DSFB.svg.generate.sh
@@ -0,0 +1,2 @@
+qrencode -t SVG -o DSFB.svg -lH 'fb://page/177131792326428/'
+# xviewer DSFB.svg
---
+++ media/instagram.svg.generate.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+qrencode -t SVG -o DSinstrgram.svg -lH 'http://instagram.com/_u/diodorsirola'
